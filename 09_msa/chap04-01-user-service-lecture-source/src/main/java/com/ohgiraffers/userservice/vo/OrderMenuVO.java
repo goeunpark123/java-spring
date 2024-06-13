9d79cdb0f8eb2bb08a1394c0c0055c4997e4d071
goeunpark123 <64297541+goeunpark123@users.noreply.github.com>
2024-06-13 17:21:56 +0900

    Add: 09_msa

---
+++ 09_msa/chap04-01-user-service-lecture-source/src/main/java/com/ohgiraffers/userservice/vo/OrderMenuVO.java
@@ -0,0 +1,11 @@
+package com.ohgiraffers.userservice.vo;
+
+import lombok.Data;
+
+@Data
+public class OrderMenuVO {
+
+    private int orderCode;
+    private int menuCode;
+    private int orderAmount;
+}
---
+++ 09_msa/chap04-02-order-service-lecture-source/src/main/java/com/ohgiraffers/orderservice/aggregate/OrderMenu.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.orderservice.aggregate;
+
+import lombok.Data;
+
+@Data
+public class OrderMenu {
+
+    private int orderCode;
+    private int menuCode;
+    private int orderAmount;
+
+
+}
